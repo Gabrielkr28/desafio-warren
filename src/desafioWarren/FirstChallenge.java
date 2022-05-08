a36ac58519586784741c5eea1f869a5c31ed679b
Gabriel Krzizanowski <gabrielkrzizanowski@gmail.com>
2022-05-07 23:46:41 -0300

    update

--- src/desafioWarren/FirstChallenge.java
+++ src/desafioWarren/FirstChallenge.java
@@ -1,40 +1,44 @@
 package desafioWarren;
 
+import java.util.ArrayList;
+
 public class FirstChallenge {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		// Lembrar que 0 + 0 não é um input aceitavel.
-		
+
 		System.out.println(NumerosImpares());
 
 	}
-
-	/*
-	 * Fazer Array com todos os valores e comparar quando aparecer o inverso dele
-	 * Para não ocorrer o problema de ele aparecer duplicado (No caso o iverso dele)
-	 * 
-	 * 
-	 * Verificar também sempre que o inverso começar com 0, negar a soma e prosseguir
-	 * Pois não é um operação valida
-	 * 
-	 * */
 	
 	public static String NumerosImpares() {
 		String numbersWithSpace = "";
 		int limite = 1000;
 		int count = 0;
+		ArrayList<Integer> resultados = new ArrayList<Integer>();
 
 		for (int i = 0; i < limite; i++) {
 			if (i < 10) {
 
 			} else {
-				String presentValue = new StringBuilder(String.valueOf(i)).reverse().toString();
-				int result = Integer.parseInt(presentValue) + i;
+				String reverseValue = new StringBuilder(String.valueOf(i)).reverse().toString();
+
+				Integer result = Integer.parseInt(reverseValue) + i;
 				if (result % 2 != 0) {
-					numbersWithSpace += i + " \n";
-					count++;
+					if (resultados.contains(result)) {
+
+					} else {
+						if (reverseValue.startsWith("0")) {
+							
+						} else {
+//							String convertedValue = new StringBuilder(result).toString();
+							resultados.add(result);
+							numbersWithSpace += i + " \n";
+							count++;
+						}
+					}
 				}
 			}
 		}
---
+++ src/desafioWarren/Teste.java
@@ -0,0 +1,23 @@
+package desafioWarren;
+
+import java.util.ArrayList;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		ArrayList<String> teste = new ArrayList<String>();
+		
+		
+		teste.add("33");
+		teste.add("10");
+		teste.add("40");
+		teste.add("33");
+		
+		System.out.println(teste.toString());
+		
+	}
+
+}
